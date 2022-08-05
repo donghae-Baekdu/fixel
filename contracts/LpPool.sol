9c438e438b8d4cf9d731aefe542de211ef48b6cd
Noah Choi <noah@baekdutech.com>
2022-08-06 00:50:14 +0900

    Design Lp pool architecture

--- contracts/LpPool.sol
+++ contracts/LpPool.sol
@@ -1,13 +1,16 @@
 pragma solidity ^0.8.9;
 
 import "./Position.sol";
+import "./LpToken.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract LpPool {
+contract LpPool is LpToken {
     function addLiquidity(uint256 marginQty)
         public
         returns (uint256 lpTokenQty)
     {
         // TODO get price
+        // TODO get fee tier of user
         // TODO check requirements; amount to transfer is less than balance
         // TODO mint amount of token
     }
@@ -19,14 +22,6 @@ contract LpPool {
         // TODO get price
         // TODO check requirements; amount to transfer is less than balance
         // TODO burn amount of token
-    }
-
-    function mint() public {
-        // TODO mint amount of token; refer synthetix kwenta
-    }
-
-    function burn() public {
-        // TODO burn amount of token; refer synthetix kwenta
     }
 
     function getPrice() public view returns (uint256 _price) {
