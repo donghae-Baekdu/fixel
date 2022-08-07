9ce53e1a4102189e63126c2768ae95d5ed3e4528
Oliver-Baekdu <oliver@baekdutech.com>
2022-08-07 17:11:26 +0900

    Add close position

--- contracts/interfaces/IPositionController.sol
+++ contracts/interfaces/IPositionController.sol
@@ -3,6 +3,16 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 
 interface IPositionController is IERC721Enumerable {
+    enum TradeType{
+        OPEN,
+        CLOSE,
+    }
+    
+    enum Status {
+        OPEN,
+        CLOSE
+    }
+
     enum Side {
         LONG,
         SHORT
@@ -19,6 +29,7 @@ interface IPositionController is IERC721Enumerable {
         uint256 margin;
         uint256 price;
         Side side;
+        Status status;
     }
 
     struct MarketStatus {
@@ -33,12 +44,19 @@ interface IPositionController is IERC721Enumerable {
 
     event ChangeMaxLeverage(uint80 marketId, uint32 _maxLeverage);
     event AddMarket(uint80 marketCount, string name, uint32 _maxLeverage);
-
+    event OpenPosition(address user, uint80 marketId, uint256 margin, uint32 leverage, Side side,uint256 tokenId);
+    event ClosePosition(address user, uint80 marketId, uint256 margin, Side side,uint256 tokenId, bool isProfit, uint256 pnl, uint256 receiveAmount);
+    
     function openPosition(
         uint80 marketId,
         uint256 liquidity,
         uint32 leverage,
         Side side
+    ) external;
+
+    function closePosition(
+        uint80 marketId,
+        uint256 tokenId
     ) external;
 
     function getMarketMaxLeverage(uint80 marketId)
@@ -53,5 +71,5 @@ interface IPositionController is IERC721Enumerable {
     function getUnrealizedPnl(uint80 marketId)
         external
         view
-        returns (Sign isPositive, uint256 pnl);
+        returns (Sign isPositive, uint256 pnl, uint256 currentPrice);
 }
