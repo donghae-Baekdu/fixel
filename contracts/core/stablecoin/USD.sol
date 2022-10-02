52fed30eb63b7fd1f7b04b4931de888810ede62e
Oliver-Baekdu <oliver@baekdutech.com>
2022-10-02 16:30:28 +0900

    Implement ERC20 functions on USD contract

--- contracts/core/stablecoin/USD.sol
+++ contracts/core/stablecoin/USD.sol
@@ -1,18 +1,22 @@
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IUSD} from "../../interfaces/IUSD.sol";
 import {IAdmin} from "../../interfaces/IAdmin.sol";
 
-contract USD is ERC20 {
+contract USD is IUSD {
     address admin;
     address public underlyingAsset;
 
+    uint public totalSupply;
+    mapping(address => uint) public balanceOf;
+    mapping(address => mapping(address => uint)) public allowance;
+    string public name = "USD Token";
+    string public symbol = "USD";
+
     uint8 _decimals = 6;
 
-    constructor(address _admin, address _underlyingAsset)
-        ERC20("USD Token", "USD")
-    {
+    constructor(address _admin, address _underlyingAsset) {
         admin = _admin;
         underlyingAsset = _underlyingAsset;
     }
@@ -20,7 +24,8 @@ contract USD is ERC20 {
     modifier checkAuthority() {
         require(
             msg.sender == IAdmin(admin).getTradePositionManager() ||
-                msg.sender == IAdmin(admin).getLpPositionManager() || msg.sender == IAdmin(admin).getVault(),
+                msg.sender == IAdmin(admin).getLpPositionManager() ||
+                msg.sender == IAdmin(admin).getVault(),
             "Sender doesn't have authority to mint"
         );
         _;
@@ -35,11 +40,41 @@ contract USD is ERC20 {
         //should this function exist?
     }
 */
+
+    function transfer(address recipient, uint amount) external returns (bool) {
+        balanceOf[msg.sender] = balanceOf[msg.sender] - amount;
+        balanceOf[recipient] = balanceOf[recipient] + amount;
+        emit Transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function approve(address spender, uint amount) external returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint amount
+    ) external returns (bool) {
+        allowance[sender][msg.sender] = allowance[sender][msg.sender] - amount;
+        balanceOf[sender] = balanceOf[sender] - amount;
+        balanceOf[recipient] = balanceOf[recipient] + amount;
+        emit Transfer(sender, recipient, amount);
+        return true;
+    }
+
     function mint(address account, uint amount) external checkAuthority {
-        _mint(account, amount);
+        balanceOf[account] = balanceOf[account] + amount;
+        totalSupply = totalSupply + amount;
+        emit Transfer(address(0), account, amount);
     }
 
     function burn(address account, uint amount) external checkAuthority {
-        _burn(account, amount);
+        balanceOf[account] = balanceOf[account] - amount;
+        totalSupply = totalSupply - amount;
+        emit Transfer(account, address(0), amount);
     }
 }
